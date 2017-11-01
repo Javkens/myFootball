04647f10b726017b8a7b2d3d42bababed59cfa83
Javkens <artur.orawiec@outlook.com>
2017-11-01 23:51:35 +0100

    InitialModel of Database

---
+++ myFootball/Migrations/201711012238224_InitialModel1.cs
@@ -0,0 +1,28 @@
+namespace myFootball.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialModel1 : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Players",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        Birthdate = c.String(),
+                        Address = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Players");
+        }
+    }
+}
