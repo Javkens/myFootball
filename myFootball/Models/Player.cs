a60a7f9796db124e8fbb66cade55926490fa1ac9
Javkens <artur.orawiec@outlook.com>
2017-11-01 23:28:57 +0100

    Prepare to add EntityFramework

--- myFootball/Models/Player.cs
+++ myFootball/Models/Player.cs
@@ -8,10 +8,7 @@ namespace myFootball.Models
     public class Player
     {
         public int Id { get; set; }
-        public int GroupId { get; set; }
-        public string FirstName { get; set; }
-        public string SecondName { get; set; }
-        public string LastName { get; set; }
+        public string Name { get; set; }
         public string Birthdate { get; set; }
         //public int WittySystemId { get; set; } aorawiec: question that is nessesery
         public string Address { get; set; }
@@ -20,21 +17,16 @@ namespace myFootball.Models
         {
             Id = _id;
             //rest of parameters should be getted from database. actually default
-            FirstName = "Jan";
-            SecondName = "Andrzej";
-            LastName = "Duda!";
+            Name = "Artur Orawiec"
             Birthdate = "1994-08-12";
             Address = "Lipnica Wielka";
 
         }
-        public Player(int _id, int _group_id, string _first, string _second, string _last, string _birth, string _address)
+        public Player(int _id, string _name, string _birth, string _address)
         {
 
             Id = _id;
-            GroupId = _group_id;
-            FirstName = _first;
-            SecondName = _second;
-            LastName = _last;
+            Name = _name;
             Birthdate = _birth;
             Address = _address;
 
