f6f4904aef019a8129475518308d9f1a49b0ad42
Javkens <artur.orawiec@outlook.com>
2017-11-01 21:31:23 +0100

    Second commit

---
+++ myFootball/Controllers/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using myFootball.Models;
+
+
+namespace myFootball.Controllers
+{
+    public class PlayerController : Controller
+    {
+        // GET: Player
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        [Route("player/mainview")]
+        public ActionResult MainView()
+        {
+            var MainView = new Player(0); //aorawiec: tutaj bedzie trzeba zainicjalizować tylko tablicę ze wszystkimi testami. nie wiem czy nie utworzyc do tego osobnej klasy
+            return View(MainView);
+
+        }
+
+
+        // GET: Test/id
+        [Route("player/{id}")]
+        public ActionResult Player(int id)
+        {
+            var player = new Player(id);
+            return View(player);
+        }
+    }
+}
---
+++ myFootball/Models/Player.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace myFootball.Models
+{
+    public class Player
+    {
+        public int Id { get; set; }
+        public int GroupId { get; set; }
+        public string FirstName { get; set; }
+        public string SecondName { get; set; }
+        public string LastName { get; set; }
+        public string Birthdate { get; set; }
+        //public int WittySystemId { get; set; } aorawiec: question that is nessesery
+        public string Address { get; set; }
+
+        public Player (int _id)
+        {
+            Id = _id;
+            //rest of parameters should be getted from database. actually default
+            FirstName = "Jan";
+            SecondName = "Andrzej";
+            LastName = "Duda!";
+            Birthdate = "1994-08-12";
+            Address = "Lipnica Wielka";
+
+        }
+        public Player(int _id, int _group_id, string _first, string _second, string _last, string _birth, string _address)
+        {
+
+            Id = _id;
+            GroupId = _group_id;
+            FirstName = _first;
+            SecondName = _second;
+            LastName = _last;
+            Birthdate = _birth;
+            Address = _address;
+
+        }
+    }
+}
