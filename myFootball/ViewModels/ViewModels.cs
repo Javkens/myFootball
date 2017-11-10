7a15ad57af53ff7632b80289fec19c3672328a65
Javkens <artur.orawiec@outlook.com>
2017-11-10 22:37:56 +0100

    Added number of players in GroupView

---
+++ myFootball/ViewModels/ViewModels.cs
@@ -0,0 +1,29 @@
+﻿using myFootball.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace myFootball.ViewModels
+{
+    public class PlayerGroup
+    {
+        public Player Player { get; set; }
+        public Group Group { get; set; }
+    }
+
+    public class PlayerEditViewModel
+    {
+        public Player Player { get; set; }
+        public IEnumerable<Group> Groups { get; set; }
+    }
+
+
+    public class GroupIndexView
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string City { get; set; }
+        public int NumberOfPlayers { get; set; }
+    }
+}
